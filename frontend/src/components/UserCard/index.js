591cd6815388a4946d0bfc762772d3cf14afa94f
luscasHenrique <lucas.henrique.r@hotmail.com>
2023-07-24 17:18:36 -0300

    aba perfil

--- frontend/src/components/UserCard/index.js
+++ frontend/src/components/UserCard/index.js
@@ -1,72 +1,101 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
 import './usercardModule.css';
 import { BsFillPersonFill, BsPencilSquare, BsEnvelopeFill, BsGearFill, BsQuestionCircleFill, BsBoxArrowRight } from 'react-icons/bs';
-import api from '../../utils/api'
+import api from '../../utils/api';
 
 // Context
 import { Context } from '../../context/UserContext';
 import { useContext } from 'react';
 
-
 function UserCard() {
-  const [user, setUser] = useState({})
-  const [token] = useState(localStorage.getItem('token') || '')
+  const [user, setUser] = useState({});
+  const [token] = useState(localStorage.getItem('token') || '');
   const [isMenuOpen, setMenuOpen] = useState(false);
 
-  const { authenticated, logout } = useContext(Context)
+  const { authenticated, logout } = useContext(Context);
+  const menuRef = useRef();
+
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  const handleProfileClick = (event) => {
+    event.stopPropagation();
+    toggleMenu();
+  };
+
+  const handleLinkClick = () => {
+    closeMenu();
+  };
+
   useEffect(() => {
-    api.get('users/checkuser',{
-    headers: {
-      Authorization: `Bearer ${JSON.parse(token)}`
+    api.get('users/checkuser', {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    })
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [token]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        closeMenu();
+      }
     }
-  })
-  .then((response) => {
-    setUser(response.data)
-  }).catch((error) => {
-    console.log(error)
-  })}, [token])
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="action">
-      <div onClick={toggleMenu}>
-      <img
-        src={
-          `${process.env.REACT_APP_API}/images/users/${user.imageName}`
-        }
-        alt={user.imageName}
-        className="profile" 
-      />
+      <div onClick={handleProfileClick}>
+        <img
+          src={`${process.env.REACT_APP_API}/images/users/${user.imageName}`}
+          alt={user.imageName}
+          className="profile"
+        />
       </div>
-      <div className={`menu ${isMenuOpen ? 'active' : ''}`}>
-        <h3>{user.name}<br/><span>Administrador</span></h3>
+      <div ref={menuRef} className={`menu ${isMenuOpen ? 'active' : ''}`}>
+        <h3>{user.name}<br /><span>Administrador</span></h3>
         <ul>
           <li>
             <BsFillPersonFill className="icon" />
-            <Link to="/profile">Meu Perfil</Link>
+            <Link to="/profile" onClick={handleLinkClick}>Meu Perfil</Link>
           </li>
           <li>
             <BsPencilSquare className="icon" />
-            <Link to="/edituser">Editar Perfil</Link>
+            <Link to="/edituser" onClick={handleLinkClick}>Editar Perfil</Link>
           </li>
           <li>
             <BsEnvelopeFill className="icon" />
-            <Link to="/">Mensagens</Link>
+            <Link to="/" onClick={handleLinkClick}>Mensagens</Link>
           </li>
           <li>
             <BsGearFill className="icon" />
-            <Link to="/">Configurações</Link>
+            <Link to="/" onClick={handleLinkClick}>Configurações</Link>
           </li>
           <li>
             <BsQuestionCircleFill className="icon" />
-            <Link to="/">Help</Link>
+            <Link to="/" onClick={handleLinkClick}>Help</Link>
           </li>
           <li>
             <BsBoxArrowRight className="icon" />
-            <Link onClick={logout}>Sair</Link>
+            <Link onClick={() => { closeMenu(); logout(); }}>Sair</Link>
           </li>
         </ul>
       </div>
